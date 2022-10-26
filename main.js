e3d304846b178f938fc16ab00e334535fb8581ef
muffinerrol <errolyeung@hotmail.com>
2022-10-26 13:42:24 +0100

    completely removed error reply on main

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		//await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		
 	}
 
 });
@@ -71,7 +71,7 @@ client.on('interactionCreate', async interaction => {
 		await response.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		//await interaction.reply({ content: 'There was an error while executing this response!', ephemeral: true });
+		
 	}
 
 });
